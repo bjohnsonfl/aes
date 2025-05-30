469ecf3b94eeccfe1c315e7b1e1525ccc0d5b0eb
Blake Johnson <blakejohnsonuf@gmail.com>
2025-05-30 12:29:12 -0400

    Aes decrypt impl (#1)
    * implemented aes decryption
    
    * added updates to aes.py and renamed it to aes_encrypt.py

--- src/python/aes_encrypt.py
+++ src/python/aes_encrypt.py
@@ -1,3 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 # AES128 Constants
 Nr = 10
 Nb = 4
@@ -20,44 +24,23 @@ s_box = [[0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b
          [0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf], 
          [0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16]]
 
-inv_s_box = [[0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb],
-             [0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb],
-             [0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e],
-             [0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25],
-             [0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92],
-             [0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84],
-             [0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06],
-             [0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b],
-             [0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73],
-             [0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e],
-             [0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b],
-             [0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4],
-             [0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f],
-             [0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef],
-             [0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61],
-             [0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d]]
-
 def print_state(state):
     for row in state:
         hex_row = [hex(x) for x in row]  
-        print(hex_row)
+        logger.debug(hex_row)
 
 def print_round_key(w):
-    print("Round_key")
+    logger.debug("Round_key")
     matrix = [ [hex((w[i] >> (24 - 8*j)) & 0xFF) for i in range(4)] for j in range(4) ]
-    [print([matrix[i][j] for j in range(4)]) for i in range(4)]
+    [logger.debug([matrix[i][j] for j in range(4)]) for i in range(4)]
  
-
-
-    
-
 def SubBytes(state):
     for i in range(4):
         for j in range(4):
             row = (state[i][j] >> 4) & 0x0F
             col = state[i][j] & 0x0F
             state[i][j] = s_box[row][col]
-    print("SubBytes")
+    logger.debug("SubBytes")
     print_state(state)
     return state
     
@@ -69,10 +52,9 @@ def ShiftRows(state):
     # the row is the same by how much you rotate
     for r in range(4):
         for c in range(4):
-            #print(f"r: {r} c: {c} new c: {(c + r) % Nb}")
             state_shift[r][c] = state[r][(c + r) % Nb]
     state = state_shift
-    print("ShiftRows")
+    logger.debug("ShiftRows")
     print_state(state)
     return state
 
@@ -90,7 +72,7 @@ def MixColumns(state):
         state_mix[2][c] = state[0][c] ^ state[1][c] ^ mult(0x02, state[2][c]) ^ mult(0x03, state[3][c])
         state_mix[3][c] = mult(0x03, state[0][c]) ^ state[1][c] ^ state[2][c] ^ mult(0x02, state[3][c])
     state = state_mix
-    print("MixColumns")
+    logger.debug("MixColumns")
     print_state(state)
     return state
 
@@ -103,33 +85,29 @@ def mult(a, b):
         tmp = xtimes(tmp)
         xtimes_lut.append(tmp)
 
-
-    #print(f"a: {hex(a)} b: {hex(b)}")
     for i in range(8):
         idx = 1 << i
         if idx & b:
-            # print(hex(idx))
             res ^= xtimes_lut[i]
-    # print(hex(res))
     return res
 
 def xtimes(a):
     tmp = (a & 0xFF) << 1
     if tmp & 0x100:
         tmp = (tmp ^ 0x11b) & 0xff
-    # print(f'a: {hex(a)} out: {hex(tmp)}')
+    # logger.debug(f'a: {hex(a)} out: {hex(tmp)}')
     return tmp
 
 def AddRoundKey(state, w):
-    #print(w)
+    #logger.debug(w)
     print_round_key(w)
     for i in range(4):
-        #print(hex(w[i]))
+        #logger.debug(hex(w[i]))
         for j in range(Nb):
-            #print(hex((w[i] >> (24 - 8*j)) & 0xFF))
+            #logger.debug(hex((w[i] >> (24 - 8*j)) & 0xFF))
             w_byte = (w[i] >> (24 - 8*j)) & 0xFF
             state[j][i] ^= w_byte
-    print("AddRoundKey")
+    logger.debug("AddRoundKey")
     print_state(state)
     return state
 
@@ -150,13 +128,7 @@ def SubWord(word):
         subword_hex.append(s_box[row][col])
     return int.from_bytes(subword_hex, 'big')
 
-
-
-
-
-
 def Cipher(text_in, w):
-    w_hex = bytes.fromhex(w)
     text_in_hex = bytes.fromhex(text_in)
     state = []
     # s[r,c]=in[r+4c] for0≤r<4 and 0≤c<Nb, (3.3)
@@ -169,11 +141,17 @@ def Cipher(text_in, w):
     
     w = KeyExpansion(w, Nk)
 
+    logger.debug("\nRound: 0")
+    logger.debug("input")
+    print_state(state)
 
     state = AddRoundKey(state, w[0:Nb])
 
     for round in range(1, Nr):
-        print(f"\nRound: {round}")
+        logger.debug(f"\nRound: {round}")
+        logger.debug("start")
+        print_state(state)
+
         state = SubBytes(state)
         state = ShiftRows(state)
         state = MixColumns(state)
@@ -184,46 +162,13 @@ def Cipher(text_in, w):
     state = AddRoundKey(state, w[Nr*Nb : ((Nr+1)*Nb)])
 
     out = bytes(state[r][c] for c in range(4) for r in range(Nb)).hex()
-    print(out)
+    logger.debug(f'out: {out}')
     return out
-
-    # out[r+4c]=s[r,c] for0≤r<4 and 0≤c<Nb. (3.4)
-    pass
-#    out = None
-#
-#    byte  state[4,Nb]
-#    state = in
-#    AddRoundKey(state, w[0, Nb-1])
-#    for round = 1 step 1 to Nr–1
-#        SubBytes(state)
-#        ShiftRows(state)
-#        MixColumns(state)
-#        AddRoundKey(state, w[round*Nb, (round+1)*Nb-1])
-#
-#    SubBytes(state)
-#    ShiftRows(state)
-#    AddRoundKey(state, w[Nr*Nb, (Nr+1)*Nb-1])
-#    out = state
-#    
-#    return out
-
-
-
-
-# Cipher Key = 2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c
-# for Nk = 4, which results in
-# w0 = 2b7e1516 w1 = 28aed2a6 w2 = abf71588
-# w3 = 09cf4f3c
-key = "2b7e151628aed2a6abf7158809cf4f3c"
-
 
 def KeyExpansion(key, Nk):
     temp = None
     w = []
     key_hex = bytes.fromhex(key)
-
-    #for i in range(Nk):
-    #    w[i] = key[4*i], key[4*i+1], key[4*i+2], key[4*i+3]
 
     w = [
         int.from_bytes(key_hex[i:i+4], 'big') for i in range(0, len(key_hex), 4)
@@ -234,25 +179,38 @@ def KeyExpansion(key, Nk):
     for i in range(2, Nb*(Nr + 1)):
         rcon_arr.append(xtimes(rcon_arr[i-1] >> 24) << 24)
 
+    logger.debug("KeyExpansion")
+    [logger.debug(f'{i//4}: {hex(w[i])}') for i in range(Nb)]
+
     for i in range(Nk, Nb * (Nr+1)):
         temp = w[i-1]
         if (i%Nk ==0):
             temp = SubWord(RotWord(temp)) ^ rcon_arr[i//Nk]
         w.append( w[i-Nk] ^ temp )
-        print(f'{i}: {hex(w[i])}')
+        logger.debug(f'{i//4}: {hex(w[i])}')
 
     return w
 
-# KeyExpansion(key, Nk)
+def test_cipher():
+    key = "2b7e151628aed2a6abf7158809cf4f3c"
+    KeyExpansion(key, Nk)
 
-# Input      = 32 43 f6 a8 88 5a 30 8d 31 31 98 a2 e0 37 07 34 
-# Cipher Key = 2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c
-input1 = "3243f6a8885a308d313198a2e0370734"
-key1   = "2b7e151628aed2a6abf7158809cf4f3c"
-out1 = "3925841d02dc09fbdc118597196a0b32" # Appendix B
-assert(Cipher(input1, key1) == out1)
+    # Input      = 32 43 f6 a8 88 5a 30 8d 31 31 98 a2 e0 37 07 34 
+    # Cipher Key = 2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c
+    input1 = "3243f6a8885a308d313198a2e0370734"
+    key1   = "2b7e151628aed2a6abf7158809cf4f3c"
+    out1 = "3925841d02dc09fbdc118597196a0b32" # Appendix B
+    assert(Cipher(input1, key1) == out1)
 
-input2 = "00112233445566778899aabbccddeeff"
-key2 = "000102030405060708090a0b0c0d0e0f"
-out2 = "69c4e0d86a7b0430d8cdb78070b4c55a" # Appendix C
-assert(Cipher(input2, key2) == out2)
+    input2 = "00112233445566778899aabbccddeeff"
+    key2 = "000102030405060708090a0b0c0d0e0f"
+    out2 = "69c4e0d86a7b0430d8cdb78070b4c55a" # Appendix C
+    assert(Cipher(input2, key2) == out2)
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    test_cipher()
