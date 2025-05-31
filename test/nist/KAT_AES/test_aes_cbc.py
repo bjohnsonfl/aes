5cb4ce38951febc6998eb292d16c376a453b6b71
Blake Johnson <blakejohnsonuf@gmail.com>
2025-05-31 00:59:17 -0400

    added swap test

--- test/nist/KAT_AES/test_aes_cbc.py
+++ test/nist/KAT_AES/test_aes_cbc.py
@@ -1,5 +1,6 @@
 from pathlib import Path 
 import pytest
+import re
 
 from src.python.aes_modes import aes_cbc
 from scripts.rsp_parser import parse_kat_file
@@ -16,6 +17,11 @@ def test_kat_aes_cbc_encrypt(kat_files):
             ct = aes_cbc(value.plaintext, value.key, value.iv, op)
             assert(ct == value.ciphertext)
 
+            # Swap PT and IV
+            ct = aes_cbc(value.iv, value.key, value.plaintext, op)
+            assert(ct == value.ciphertext)
+
+
 @pytest.mark.parametrize("kat_files", get_kat_cbc_files(), ids=lambda p: p.name)
 def test_kat_aes_cbc_decrypt(kat_files):
     kat_list = parse_kat_file(kat_files)
@@ -23,3 +29,7 @@ def test_kat_aes_cbc_decrypt(kat_files):
         if op == "DECRYPT":
             pt = aes_cbc(value.ciphertext, value.key, value.iv, op)
             assert(pt == value.plaintext)
+
+            # Swap CT and IV
+            pt = aes_cbc(value.ciphertext, value.key, value.plaintext, op)
+            assert(pt == value.iv)
