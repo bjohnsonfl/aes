04face03470787895ab229b0fbfbb8a9497896cb
Blake Johnson <blakejohnsonuf@gmail.com>
2025-05-31 23:12:46 -0400

    refactored test parameters

--- test/nist/aesmmt/test_aes_cbc.py
+++ test/nist/aesmmt/test_aes_cbc.py
@@ -4,21 +4,22 @@ import pytest
 from src.python.aes_modes import aes_cbc
 from scripts.rsp_parser import parse_kat_file
 
-def get_mmt_cbc_files():
-    return list(Path("nist/aesmmt").glob("CBC*.rsp"))
+def get_mmt_cbc_files(operation):
+    mmt_files = (list(Path("nist/aesmmt").glob("CBC*.rsp")))
+    tests = []
+    for file in mmt_files:
+        mmt_list = parse_kat_file(file)
+        for (op, count), test in mmt_list.items():
+            if op == operation:
+                tests.append((file, count, test))
+    return tests
 
-@pytest.mark.parametrize("mmt_files", get_mmt_cbc_files(), ids=lambda p: p.name)
-def test_mmt_aes_cbc_encrypt(mmt_files):
-    mmt_list = parse_kat_file(mmt_files)
-    for (op, count), value in mmt_list.items():
-        if op == "ENCRYPT":
-            ct = aes_cbc(value.plaintext, value.key, value.iv, op)
-            assert(ct == value.ciphertext)
+@pytest.mark.parametrize(("file, count, test"), get_mmt_cbc_files("ENCRYPT"), ids=[f'{x[0].stem}_{x[2].operation}_{x[1]}' for x in get_mmt_cbc_files("ENCRYPT")])
+def test_mmt_aes_cbc_encrypt(file, count, test):
+    ct = aes_cbc(test.plaintext, test.key, test.iv, test.operation)
+    assert(ct == test.ciphertext)
 
-@pytest.mark.parametrize("mmt_files", get_mmt_cbc_files(), ids=lambda p: p.name)
-def test_mmt_aes_cbc_decrypt(mmt_files):
-    mmt_list = parse_kat_file(mmt_files)
-    for (op, count), value in mmt_list.items():
-        if op == "DECRYPT":
-            pt = aes_cbc(value.ciphertext, value.key, value.iv, op)
-            assert(pt == value.plaintext)
+@pytest.mark.parametrize(("file, count, test"), get_mmt_cbc_files("DECRYPT"), ids=[f'{x[0].stem}_{x[2].operation}_{x[1]}' for x in get_mmt_cbc_files("DECRYPT")])
+def test_mmt_aes_cbc_decrypt(file, count, test):
+    pt = aes_cbc(test.ciphertext, test.key, test.iv, test.operation)
+    assert(pt == test.plaintext)
